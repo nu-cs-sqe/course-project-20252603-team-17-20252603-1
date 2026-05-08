c21b01f688094a912f2b00cae79eb54758c7e045
tahaazeem2026 <tahaazeem2026@u.northwestern.edu>
2026-05-07 20:53:54 -0500

    initial test failing, but compiling, and piece class scaffold/placeholder created

--- src/main/java/Board/Board.java
+++ src/main/java/Board/Board.java
@@ -1,4 +1,11 @@
 package Board;
 
 public class Board {
+    public void initializeBoard() {
+        return;
+    }
+
+    public Piece getPieceAt(int row, int col) {
+        return new Piece("NULL", null);
+    }
 }
