fcd1c3461033ed1c31feef4fc313bf24e30b4233
tahaazeem2026 <tahaazeem2026@u.northwestern.edu>
2026-05-07 20:53:54 -0500

    initial test failing, but compiling, and piece class scaffold/placeholder created

--- src/test/java/BoardTests/BoardTests.java
+++ src/test/java/BoardTests/BoardTests.java
@@ -1,4 +1,28 @@
 package BoardTests;
 
+import Board.Board;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BoardTests {
+    @Test
+    void initEmptyBoardGeneratesInternalState() {
+        Board board = new Board();
+
+        board.initializeBoard();
+
+        int pieceCount = 0;
+
+        for(int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                if (board.getPieceAt(row, col) != null) {
+                    pieceCount++;
+                }
+            }
+        }
+
+        assertEquals(32, pieceCount);
+    }
+
 }
