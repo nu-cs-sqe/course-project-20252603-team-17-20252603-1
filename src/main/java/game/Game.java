12a31c5ca73550b7538f6016407006b44c0978fd
Saeed AlKaabi <saeedalkaabi2027@u.northwestern.edu>
2026-05-10 18:00:03 -0500

    Add Game start test, startNewGameSetsCurrentPlayerToWhite pass

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -1,4 +1,16 @@
 package game;
 
+import player.Player;
+
 public class Game {
+
+    private Player currentPlayer;
+
+    public void startNewGame() {
+        currentPlayer = new Player("White Player", "WHITE");
+    }
+
+    public Player getCurrentPlayer() {
+        return currentPlayer;
+    }
 }
