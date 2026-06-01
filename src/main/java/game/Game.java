2fda15439912c634d767e97db18fb56d6edb3b20
Saeed AlKaabi <saeedalkaabi2027@u.northwestern.edu>
2026-05-31 19:49:42 -0500

    Detect rook check on king

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -3,6 +3,7 @@ import board.Piece;
 
 import player.Player;
 import board.Board;
+import board.Piece;
 
 public class Game {
 
@@ -91,9 +92,78 @@ public class Game {
         return true;
     }
 
+
     public boolean isKingInCheck(String color) {
+        if (board == null) {
+            return false;
+        }
+
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                Piece piece = board.getPieceAt(row, col);
+
+                if (piece != null
+                        && "KING".equals(piece.getType())
+                        && color.equals(piece.getColor())) {
+                    String opponentColor = "WHITE".equals(color) ? "BLACK" : "WHITE";
+                    return isSquareUnderAttack(row, col, opponentColor);
+                }
+            }
+        }
+
         return false;
     }
+
+    public boolean isSquareUnderAttack(int row, int col, String byColor) {
+        if (board == null || !board.isWithinBounds(row, col)) {
+            return false;
+        }
+
+        for (int startRow = 0; startRow < 8; startRow++) {
+            for (int startCol = 0; startCol < 8; startCol++) {
+                Piece piece = board.getPieceAt(startRow, startCol);
+
+                if (piece != null
+                        && byColor.equals(piece.getColor())
+                        && "ROOK".equals(piece.getType())
+                        && rookAttacksSquare(startRow, startCol, row, col)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean rookAttacksSquare(int startRow, int startCol, int targetRow, int targetCol) {
+        if (startRow != targetRow && startCol != targetCol) {
+            return false;
+        }
+
+        return isPathClear(startRow, startCol, targetRow, targetCol);
+    }
+
+    private boolean isPathClear(int startRow, int startCol, int endRow, int endCol) {
+        int rowStep = Integer.compare(endRow, startRow);
+        int colStep = Integer.compare(endCol, startCol);
+
+        int row = startRow + rowStep;
+        int col = startCol + colStep;
+
+        while (row != endRow || col != endCol) {
+            if (board.getPieceAt(row, col) != null) {
+                return false;
+            }
+
+            row += rowStep;
+            col += colStep;
+        }
+
+        return true;
+    }
+
+
+
 
 
 
