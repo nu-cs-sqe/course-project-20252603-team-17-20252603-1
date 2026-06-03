3986ec585c421193dd0163870e7cdd25ad92cfef
Saeed AlKaabi <saeedalkaabi2027@u.northwestern.edu>
2026-06-02 20:32:28 -0500

    Reject en passant after non-double pawn move

--- src/test/java/game/EnPassantTests.java
+++ src/test/java/game/EnPassantTests.java
@@ -177,6 +177,38 @@ public class EnPassantTests {
         assertEquals("BLACK", game.getCurrentPlayer().getColor());
     }
 
+    @Test
+    void enPassantRejectedWhenPreviousMoveWasNotTwoSquarePawnMove() {
+        Game game = new Game();
+        game.startNewGame();
+        Board board = game.getBoard();
+        clearBoard(board);
+
+        placePiece(board, 7, 4, new Piece("KING", "WHITE"));
+        placePiece(board, 0, 4, new Piece("KING", "BLACK"));
+        placePiece(board, 3, 4, new Piece("PAWN", "WHITE"));
+        placePiece(board, 2, 3, new Piece("PAWN", "BLACK"));
+
+        game.switchTurn();
+
+        assertTrue(game.makeMove(2, 3, 3, 3));
+        assertFalse(game.makeMove(3, 4, 2, 3));
+
+        Piece whitePawn = board.getPieceAt(3, 4);
+        assertNotNull(whitePawn);
+        assertEquals("PAWN", whitePawn.getType());
+        assertEquals("WHITE", whitePawn.getColor());
+
+        Piece blackPawn = board.getPieceAt(3, 3);
+        assertNotNull(blackPawn);
+        assertEquals("PAWN", blackPawn.getType());
+        assertEquals("BLACK", blackPawn.getColor());
+
+        assertNull(board.getPieceAt(2, 3));
+        assertEquals("WHITE", game.getCurrentPlayer().getColor());
+    }
+
+
 
 
 }
