b6dcd79dcf6ecd77f6cb5191a3b269fa508c877e
Saeed AlKaabi <saeedalkaabi2027@u.northwestern.edu>
2026-06-02 20:36:49 -0500

    Keep en passant available after invalid move

--- src/test/java/game/EnPassantTests.java
+++ src/test/java/game/EnPassantTests.java
@@ -302,6 +302,36 @@ public class EnPassantTests {
         assertEquals("WHITE", game.getCurrentPlayer().getColor());
     }
 
+    @Test
+    void invalidMoveDoesNotExpireEnPassantOpportunity() {
+        Game game = new Game();
+        game.startNewGame();
+        Board board = game.getBoard();
+        clearBoard(board);
+
+        placePiece(board, 7, 4, new Piece("KING", "WHITE"));
+        placePiece(board, 0, 4, new Piece("KING", "BLACK"));
+        placePiece(board, 3, 4, new Piece("PAWN", "WHITE"));
+        placePiece(board, 1, 3, new Piece("PAWN", "BLACK"));
+
+        game.switchTurn();
+
+        assertTrue(game.makeMove(1, 3, 3, 3));
+        assertFalse(game.makeMove(3, 4, 3, 5));
+        assertTrue(game.makeMove(3, 4, 2, 3));
+
+        assertNull(board.getPieceAt(3, 4));
+        assertNull(board.getPieceAt(3, 3));
+
+        Piece whitePawn = board.getPieceAt(2, 3);
+        assertNotNull(whitePawn);
+        assertEquals("PAWN", whitePawn.getType());
+        assertEquals("WHITE", whitePawn.getColor());
+        assertEquals("BLACK", game.getCurrentPlayer().getColor());
+    }
+
+
+
 
 
 
