fb45d9a7af4ef9bfd2e181c9a4eaa03c675d6006
mukhtarhandulle <mhandulle22@gmail.com>
2026-06-08 02:22:23 -0500

    Add Swing window shell for GUI mode

--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -2,11 +2,21 @@ package ui;
 
 import ui.console.ConsoleChessRunner;
 import ui.controller.GameController;
+import ui.swing.ChessWindow;
+
+import javax.swing.SwingUtilities;
 
 public class Main {
 
 	public static void main(String[] args) {
-		GameController controller = new GameController();
-		new ConsoleChessRunner(controller).run();
+		if (args.length > 0 && args[0].equalsIgnoreCase("gui")) {
+			SwingUtilities.invokeLater(() -> {
+				ChessWindow window = new ChessWindow(new GameController());
+				window.setVisible(true);
+			});
+		} else {
+			GameController controller = new GameController();
+			new ConsoleChessRunner(controller).run();
+		}
 	}
 }
